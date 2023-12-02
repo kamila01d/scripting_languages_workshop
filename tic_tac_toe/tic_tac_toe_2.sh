ca68dd1ac68a0172fb301fc349851259aeacbf32
kamila01d <kamilalakd@gmail.com>
2023-12-02 15:37:34 +0100

    zadanie na 4

--- tic_tac_toe/tic_tac_toe_2.sh
+++ tic_tac_toe/tic_tac_toe_2.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
-declare -A board
+declare -g json_file
+json_file="games.json"
 
-for ((i=0; i<9; i++)); do
-    board[$i]=" "
-done
 
+
+check_if_file_exists()  {
+  if [ ! -f "$json_file" ]; then
+    # File does not exist, create and initialize it
+    echo '{"games": []}' > "$json_file"
+  fi
+}
+
+check_if_file_not_empty() {
+    num_objects=$(jq '.games | length' "$json_file")
+    if [ "$num_objects" -gt 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
 declare_board() {
 
-  board_str=$1
   declare -g board
   board=()
-  echo "${#board_str}"
-
-
-    for ((i = 0; i < ${#board_str}; i++)); do
-      board+=("${board_str:i:1}")
-    done
-  for element in "${board[@]}"; do
-    echo "$element"
+  for ((i=0; i<9; i++)); do
+      board[$i]=" "
   done
 
 }
-
-#initialize_board()  {
-#  declare -g board
-#}
 print_board() {
-
 
     for ((i=0; i<=6; i+=3)); do
         echo "  "${board["$i"]}" | "${board["$((i+1))"]}" | "${board["$((i+2))"]}"  "
@@ -37,6 +39,7 @@ print_board() {
           echo " -----------"
         fi
     done
+
 }
 
 check_win() {
@@ -66,7 +69,6 @@ check_win() {
 }
 
 check_draw() {
-
     for ((i=0; i<=8; i+=1)); do
         if [ "${board[$i]}" == " " ];
         then
@@ -75,13 +77,11 @@ check_draw() {
     done
 
     return 0
-
 }
 
 is_single_digit_in_range() {
 
   local str=$1
-
   if [[ "$str" =~ ^[0-8]$ ]]; then
       return 0
   else
@@ -93,11 +93,9 @@ is_single_digit_in_range() {
 check_non_empty_string() {
     local str=$1
 
-    # Check if the string is not empty and has a minimum length of 1
     if [ -n "$str" ] && [ ${#str} -ge 1 ]; then
         # Check if the string contains only numbers and letters
         if [[ "$str" =~ ^[0-9a-zA-Z]+$ ]]; then
-            echo "String \"$str\" is not empty, has a minimum length of 1, and contains only numbers and letters."
             return 0
         else
             echo "String \"$str\" contains characters other than numbers and letters."
@@ -112,81 +110,142 @@ check_non_empty_string() {
 
 save_game() {
   local first_player="$1"
-  local seond_player="$2"
+  local second_player="$2"
   local turn="$3"
   local id="$4"
-  echo "board -----"
-  echo "$Board od 0 to:    "
-  echo "${board[0]}"
 
-  if [ -f your_json_file.json ]; then
-        existing_data=$(cat your_json_file.json)
-        if [ -z "${existing_data}" ]; then
-          existing_data='{"games": []}'
-        fi
-
-    else
-
-        existing_data='{"games": []}'
+  if [ -f "$json_file" ]; then
+    existing_data=$(cat "$json_file")
+    if [ -z "${existing_data}" ]; then
+      existing_data='{"games": []}'
     fi
-  echo "hello"
-  echo "${existing_data}"
+  else
+    existing_data='{"games": []}'
+  fi
+
+  current_date=$(date "+%Y-%m-%d %H:%M:%S")
 
   json_array="["
   for ((i=0; i<9; i++)); do
-    echo "${board[$i]}"
     element="${board[$i]}"
     json_array+="\"$element\","
   done
-  json_array="${json_array%,}"  # Remove the trailing comma
+  json_array="${json_array%,}"
   json_array+="]"
 
+  new_object="{\"id\": \"${id}\", \"first_player\": \"${first_player}\",\"second_player\": \"${second_player}\", \"turn\": \"${turn}\", \"date\": \"${current_date}\", \"my_string\": ${json_array}}"
 
-  new_object="{\"id\": \"${id}\", \"first_player\": \"${first_player}\",\"second_player\": \"${second_player}\", \"turn\": \"${turn}\", \"my_string\": ${json_array}}"
-  echo "${new_object}"
+  result=$(echo "$existing_data" | jq --arg target_id "$id" '.games[] | select(.id == $target_id) | .id')
 
-  array_part=$(echo "$existing_data" | jq '.games')
-
-  if [ "$array_part" == "null" ]; then
+  if [ -n "$result" ]; then
+    updated_data=$(echo "$existing_data" | jq --argjson new_object "$new_object" --arg id "$id" '.games |= map(if .id == $id then $new_object else . end)')
+  else
+    # ID does not exist, append a new object
+    array_part=$(echo "$existing_data" | jq '.games')
+    if [ "$array_part" == "null" ]; then
       array_part="[]"
+    fi
+    updated_array=$(echo "$array_part" | jq --argjson new_object "$new_object" '. + [$new_object]')
+    updated_data=$(echo "$existing_data" | jq --argjson updated_array "$updated_array" '.games = $updated_array')
   fi
 
-  updated_array=$(echo "$array_part" | jq --argjson new_object "$new_object" '. + [$new_object]')
-
-  updated_data=$(echo "$existing_data" | jq --argjson updated_array "$updated_array" '.games = $updated_array')
-
-  echo "$updated_data" > your_json_file.json
+  echo "$updated_data" > "$json_file"
 
 }
 
 start_game()  {
-    echo 'Welcome in game Tic Tac Toe!'
-    echo '1. Start new game.'
-    echo '2. Load game'
-    echo '3. Quit'
-    read choose
-    if [ "$choose" -eq 1 ]; then
-      initialize_new_game
-    fi
 
-    if [ "$choose" -eq 2 ]; then
-      echo "Which game you want to play?"
-      read id
-      play_saved_game $id
+    echo '----- Tic Tac Toe Game -----'
+
+    while true; do
+
+      echo '1. Start new game.'
+      echo '2. Load game'
+      echo '3. Quit'
+      read choose
+      if [ "$choose" -eq 1 ]; then
+        initialize_new_game
+
+      elif [ "$choose" -eq 2 ]; then
+        if ! check_if_file_not_empty; then
+          echo "No saved games available"
+          continue
+        fi
+        echo "Which game you want to play?"
+        show_games
+        while true; do
+          echo "Choose id of the game"
+          read id
+          check_id_exists "$id"
+
+          if check_id_exists "$id"; then
+              play_saved_game $id
+              break
+              return 0
+          else
+            echo "This id does not exist"
+          fi
+        done
+
+      elif [ "$choose" -eq 3 ]; then
+        echo "Bye!"
+        exit
+
+      else
+        echo "Expect input is [1-3]"
     fi
+    done
+    return 0
 }
 
 generate_random_number() {
     echo $((RANDOM % 1000))  # Adjust the range as needed
 }
+check_id_exists() {
 
+    local id="$1"
 
-number_exists_in_json() {
-    local json_file="$1"
-    local number="$2"
-    jq -e --arg number "$number" '.games[].id == $number' "$json_file" >/dev/null
+    if [ -f "$json_file" ]; then
+      existing_data=$(cat "$json_file")
+    else
+      return 1
+    fi
+
+    result=$(echo "$existing_data" | jq --arg target_id "$id" '.games[] | select(.id == $target_id) | .id')
+    if [ -n "$result" ]; then
+      return 0
+      echo "Updated existing object with ID $id."
+    else
+      return 1
+    fi
 }
 
+show_games()  {
+  ids=$(jq -r '.games[].id' "$json_file")
+  date=$(jq -r '.games[].date' "$json_file")
+  echo "Available saved games: "
+  jq -r '.games[] | "ID: \(.id), date of the last play: \(.date)"' "$json_file"
+}
+
+delete_game_from_file() {
+  local target_id="$1"
+
+  if [ -f "$json_file" ]; then
+    existing_data=$(cat "$json_file")
+    if [ -z "${existing_data}" ]; then
+      return
+    fi
+  fi
+
+  result=$(echo "$existing_data" | jq --arg target_id "$target_id" '.games[] | select(.id == $target_id) | .id')
+
+  if [ -n "$result" ]; then
+    updated_data=$(echo "$existing_data" | jq --arg target_id "$target_id" '.games = (.games | map(select(.id != $target_id)))')
+
+    echo "$updated_data" > "$json_file"
+  fi
+
+}
 
 initialize_new_game() {
 
@@ -207,53 +266,38 @@ initialize_new_game() {
     fi
   done
 
-
   echo "Hello, $second_player!"
-
 
   player_turn="$first_player"
   mark="x"
 
   while true; do
     random_number=$(generate_random_number)
-    if ! number_exists_in_json <(echo "$your_json_file.json") "$random_number"; then
-        echo "Generated unique random number: $random_number"
+    if ! check_id_exists "$random_number"; then
         break
     fi
   done
 
-  echo "${random_number}"
-
-  play "$player_turn" "$mark" $random_number
+  declare_board
+  play "$player_turn" "$mark" "$random_number" "$second_player"
 
 }
 
 play_saved_game() {
 
   local game_id="$1"
-
-  local game_info=$(jq --arg id "$game_id" '.games[] | select(.id == $id)' "your_json_file.json")
-
-
+  local game_info=$(jq --arg id "$game_id" '.games[] | select(.id == $id)' "$json_file")
   first_player=$(echo "$game_info" | jq -r '.first_player')
   second_player=$(echo "$game_info" | jq -r '.second_player')
   turn=$(echo "$game_info" | jq -r '.turn')
 
-  my_string_json=$(jq --arg id "$id" -r '.games[] | select(.id == $id).my_string' "your_json_file.json")
-
-
+  my_string_json=$(jq --arg id "$id" -r '.games[] | select(.id == $id).my_string' "$json_file")
   my_string=$(echo "$my_string_json" | jq -c -r 'map(if . == "" then " " else . end) | @sh')
 
-
   eval "my_string=($my_string)"
-
-
-  printf "%s\n" "${my_string[@]}"
-  printf "%s" "${my_string[@]}"
   echo "Game ID: $game_id"
   echo "First Player: $first_player"
   echo "Second Player: $second_player"
-  echo "Turn: $turn"
 
   declare -g board
   board=()
@@ -264,40 +308,39 @@ play_saved_game() {
     if [ "${my_string[$i]}" == "x" ] || [ "${my_string[$i]}" == "o" ]; then
       board[$i]="${my_string[$i]}"
     fi
-    echo "${board[$i]}"
   done
 
-  if [ "${turn}" == 'x' ]; then
-    play "${first_player}" "${turn}" "${game_id}" "${second_player}"
-
-  else
-    play "${second_player}" "${turn}" "${game_id}" "${first_player}"
-  fi
+  play "${first_player}" "${turn}" "${game_id}" "${second_player}"
 
 
+  return 0
 }
 
 play () {
-  local player_turn="$1"
+  local first_player="$1"
   local mark="$2"
   local id="$3"
   local second_player="$4"
 
+  if [ "$mark" == 'x' ]; then
+    player_turn="$first_player"
+  else
+    player_turn="$second_player"
+  fi
 
+  print_board
 
   while true; do
 
-
-
       while true; do
-          echo "\n"
-          print_board
-          echo "$player_turn, please type location of your x in board  from 0 to 8 or 's' to save the current game":
+          echo "$player_turn, please type location of your $mark in board  from 0 to 8 or 's' to save the current game":
           read input
 
           if [ "$input" == "s" ]; then
               save_game "${first_player}" "${second_player}" "${mark}" "${id}"
+              start_game
               return 0
+
           fi
 
           if ! is_single_digit_in_range "$input"; then
@@ -316,11 +359,15 @@ play () {
       print_board
       if check_win "${mark}"; then
           echo "$player_turn won!!! Congratulations"
+          delete_game_from_file "$id"
+          start_game
           return 0
       fi
 
       if check_draw; then
         echo " ---------- DRAW ------------"
+        delete_game_from_file "$id"
+        start_game
         return 0
       fi
 
@@ -333,11 +380,9 @@ play () {
       fi
 
   done
+  return 0
 
 }
 
-
+check_if_file_exists
 start_game
-
-
-
